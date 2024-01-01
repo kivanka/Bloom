f04e1905d1b0a2cfa9dee8171d17b19a41711046
Ilya Lobanovskiy <kivaanka1@mail.ru>
2024-01-02 01:06:08 +0300

    feat: add index for controllers

---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+export * as UserController from './UserController.js';
+export * as ProductController from './ProductController.js';
+export * as OrderController from './ProductController.js';
+export * as CategoryContoller from './CategoryContoller.js';
