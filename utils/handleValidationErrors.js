1fd8df0927ba1d5e0a1b85e43cb7032677893f8a
Ilya Lobanovskiy <kivaanka1@mail.ru>
2024-01-02 02:02:26 +0300

    refactor: remove white spaces

--- utils/handleValidationErrors.js
+++ utils/handleValidationErrors.js
@@ -1,8 +1,8 @@
 import { validationResult } from "express-validator";
 
-export default ( req, res, next ) => {
+export default (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(400).json(errors.array());
     }
 
