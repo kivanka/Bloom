2b611fcd5966f5c776319153d75fe391178155fd
Ilya Lobanovskiy <kivaanka1@mail.ru>
2024-01-02 19:15:08 +0300

    fix: change remobing function

--- controllers/CategoryContoller.js
+++ controllers/CategoryContoller.js
@@ -51,10 +51,11 @@ export const update = async (req, res) => {
 
 export const remove = async (req, res) => {
     try {
-        const category = await CategoryModel.findByIdAndRemove(req.params.id);
+        const category = await CategoryModel.findById(req.params.id);
         if (!category) {
             return res.status(404).json({ message: 'Category not found' });
         }
+        await CategoryModel.deleteOne({ _id: req.params.id });
         res.json({ message: 'Category successfully deleted' });
     } catch (err) {
         res.status(500).json({ message: err.message });
