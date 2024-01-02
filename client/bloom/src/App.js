324ae811badbd266f2af5e8f9b6b51826b043641
Ilya Lobanovskiy <kivaanka1@mail.ru>
2024-01-02 23:02:25 +0300

    feat: add local storage for user token

--- client/bloom/src/App.js
+++ client/bloom/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAuthMe, selectIsAuth } from './redux/slices/auth';
+import { CssBaseline, Box } from '@mui/material';
 import './App.css';
 
+import Header from './components/header';
+import RegistrationPage from './components/register';
+import LoginPage from './components/login';
+
 function App() {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  React.useEffect(() => {
+    if (window.localStorage.getItem('token')) {
+      dispatch(fetchAuthMe());
+    }
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ overflowX: 'hidden' }}>
+      <CssBaseline />
+      <Box display="flex" flexDirection="column" minHeight="100vh">
+        <Header />
+        <Box component="main" flexGrow={1} sx={{ width: '100%' }}>
+          <Routes>
+              {!isAuth && <Route path='/registration' element={<RegistrationPage/>} />}
+              {!isAuth && <Route path='/login' element={<LoginPage/>} />}
+              {isAuth && <Route path="*" element={<Navigate to="/" />} />}
+              {!isAuth && <Route path="*" element={<Navigate to="/login" />} />}
+          </Routes>
+        </Box>
+      </Box>
     </div>
   );
 }
